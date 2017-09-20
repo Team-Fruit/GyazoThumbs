cc6ec90b7721f50dea03b51d7eba6cb37bd1037a
sjcl <fruit@bebehp.com>
2017-09-20 15:02:32 +0900

    log4j2

--- src/main/java/net/teamfruit/gyazothumbs/Log.java
+++ src/main/java/net/teamfruit/gyazothumbs/Log.java
@@ -1,7 +1,7 @@
 package net.teamfruit.gyazothumbs;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Log {
 	public static final Logger LOG = LogManager.getLogger(GyazoThumbs.class);
